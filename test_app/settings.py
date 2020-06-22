7e8c9c7fc46ad88986b156daf7abc337e355ca15
hideaki kondo <hdaknd@gmail.com>
2020-06-22 13:02:18 +0900

    add requirements

--- test_app/settings.py
+++ test_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v(%k7%&a)8r4*0ve@_)#k=w@anjzye^(h4@q#mtqz&rs&^9p9('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 
@@ -72,16 +72,24 @@ WSGI_APPLICATION = 'test_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dev',
-        'USER': 'root',
-        'HOST': 'db',
-        'PORT': '3306'
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'dev',
+#        'USER': 'root',
+#        'HOST': 'db',
+#        'PORT': '3306'
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
